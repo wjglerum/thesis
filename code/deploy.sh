688ba3bb84e60ebedb752ee5dc29ffdf11ee0f75
Willem Jan Glerum <wjglerum@users.noreply.github.com>
2018-07-26 15:05:52 +0200

    Update deploy.sh

--- code/deploy.sh
+++ code/deploy.sh
@@ -3,8 +3,7 @@ $ cd dcos-vagrant
 $ cp VagrantConfig-1m-1a-1p.yaml VagrantConfig.yaml
 $ vagrant up
 $ dcos cluster setup m1.dcos
-$ dcos package install etcd --yes
-    --options=etcd.json
+$ dcos package install etcd --yes --options=etcd.json
 $ dcos package install calico --yes
 $ dcos package install kubernetes --yes \
     --package-version=1.0.3-1.9.7 \
